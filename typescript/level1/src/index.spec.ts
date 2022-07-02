67c00199b572b5eb43499ddefb55b14564f248a9
HugoChea <hugochea.pro@outlook.com>
2022-07-02 22:49:20 +0200

    level1: read input file

--- typescript/level1/src/index.spec.ts
+++ typescript/level1/src/index.spec.ts
@@ -1,7 +1,68 @@
 import { main } from './index.js';
+import { loadInput } from './index.js';
+import { promises as fs } from 'fs';
 
 describe(main, () => {
   test("dummy", () => {
     expect(typeof main).toBe('function');
   });
+});
+
+describe(loadInput, () => {
+
+  const readFileSpy = jest.spyOn(fs, 'readFile');
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("it should return input file content as JSON", async () => {
+
+    const validJSON = {
+      property: "value"
+    }
+    const validFileText = JSON.stringify(validJSON);
+    readFileSpy.mockResolvedValue(validFileText);
+    try {
+      let inputData: JSON = await loadInput('input.json');
+      expect(readFileSpy).toHaveBeenCalledTimes(1);
+      expect(inputData).toStrictEqual(validJSON);
+    } catch (error) {
+      fail('it should not reach here');
+    }
+
+  });
+
+  test("it should return an error because of invalid input format", async () => {
+
+    const jsonParseSpy = jest.spyOn(JSON, 'parse');
+
+    const invalidFileText = "";
+    readFileSpy.mockResolvedValue(invalidFileText);
+    jsonParseSpy.mockRejectedValue("SyntaxError: Unexpected end of JSON input")
+
+    try {
+      let inputData: JSON = await loadInput('input.json');
+      fail('it should not reach here');
+    }
+    catch (error) {
+      expect(readFileSpy).toHaveBeenCalledTimes(1);
+      expect(jsonParseSpy).toHaveBeenCalledWith(invalidFileText);
+      expect(error).toBe("SyntaxError: Unexpected end of JSON input")
+    }
+
+  });
+
+  test("it should return an error because file not found", async () => {
+    readFileSpy.mockRejectedValue("Error: ENOENT: no such file or directory")
+    try {
+      let inputData: JSON = await loadInput('not-found-input.json');
+      fail('it should not reach here');
+    }
+    catch (error) {
+      expect(readFileSpy).toHaveBeenCalledTimes(1);
+      expect(error).toBe("Error: ENOENT: no such file or directory")
+    }
+  });
+
 });
--- typescript/level1/src/index.ts
+++ typescript/level1/src/index.ts
@@ -1,8 +1,44 @@
 import { fileURLToPath } from 'node:url';
 import * as process from 'node:process';
+import { promises as fs } from 'fs';
 
-export function main() {
-  console.log('Hello World!');
+/**
+ * Read json file as input containing :
+ * - articles
+ * - carts of customer with article id and quantity
+ * 
+ * Generate file with each carts and their total
+ */
+export async function main() {
+  try {
+    const inputData: JSON = await loadInput("input.json");
+    console.log(inputData);
+
+    // loop through articles
+    // loop through carts
+    // calculate total by article
+    // calculate total by cart
+    // generate output and write file
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+/**
+ * Take filename string as parameter and read it as utf8 string
+ * Return content as JSON
+ * @param filename 
+ * @returns 
+ */
+export async function loadInput(filename: string) {
+  try {
+    const data = await fs.readFile(filename, "utf8");
+    return JSON.parse(data);
+  }
+  catch (error) {
+    throw error;
+  }
 }
 
 if (import.meta.url != null && process.argv[1] === fileURLToPath(import.meta.url)) {
